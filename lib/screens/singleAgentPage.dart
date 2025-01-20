3a7279bc6c81fc5c4e4237bf79962e8580a549dc
jospin6 <jospinndagano1@gmail.com>
2025-01-20 08:51:38 +0200

    create single agent page

---
+++ lib/screens/singleAgentPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SingleAgentPage extends StatefulWidget {
+  const SingleAgentPage({super.key});
+
+  @override
+  State<SingleAgentPage> createState() => _SingleAgentPageState();
+}
+
+class _SingleAgentPageState extends State<SingleAgentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
