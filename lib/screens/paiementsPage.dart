0289748a99f5e5efb7fe923a94f7b5f9b4877ccc
jospin6 <jospinndagano1@gmail.com>
2025-01-20 15:47:09 +0200

    create paiementpage

---
+++ lib/screens/paiementsPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaiementsPage extends StatefulWidget {
+  const PaiementsPage({super.key});
+
+  @override
+  State<PaiementsPage> createState() => _PaiementsPageState();
+}
+
+class _PaiementsPageState extends State<PaiementsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
