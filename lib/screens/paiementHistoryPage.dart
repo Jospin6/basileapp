f333b7522fb095d4d1a5499f3c8fdd0b4298f143
jospin6 <jospinndagano1@gmail.com>
2025-01-20 15:48:37 +0200

    create paiementhistorypage

---
+++ lib/screens/paiementHistoryPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaiementHistoryPage extends StatefulWidget {
+  const PaiementHistoryPage({super.key});
+
+  @override
+  State<PaiementHistoryPage> createState() => _PaiementHistoryPageState();
+}
+
+class _PaiementHistoryPageState extends State<PaiementHistoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
