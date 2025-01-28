9b8e0f18445fea688d9cf3a762d2294d2f5962e7
jospin6 <jospinndagano1@gmail.com>
2025-01-28 11:06:45 +0200

    fix bug related to the agent history tab

--- lib/db/database_helper.dart
+++ lib/db/database_helper.dart
@@ -445,11 +445,10 @@ class DatabaseHelper {
       String agentId) async {
     final db = await database;
     return await db.rawQuery('''
-      SELECT paiements_history.*, clients.name AS client_name, taxes.amount AS tax_amount, agents.name AS agent_name  
+      SELECT paiements_history.*, clients.name AS client_name, taxes.amount AS tax_amount  
       FROM paiements_history  
       LEFT JOIN clients ON paiements_history.id_client = clients.id  
-      LEFT JOIN taxes ON paiements_history.id_taxe = taxes.id  
-      LEFT JOIN agents ON paiements_history.id_agent = agents.id  
+      LEFT JOIN taxes ON paiements_history.id_taxe = taxes.id 
       WHERE paiements_history.id_agent = ?  
     ''', [agentId]);
   }
--- lib/widgets/agentHistoryTab.dart
+++ lib/widgets/agentHistoryTab.dart
@@ -11,44 +11,42 @@ class AgentHistoryTab extends StatefulWidget {
 
 class _AgentHistoryTabState extends State<AgentHistoryTab> {
   DatabaseHelper dbHelper = DatabaseHelper();
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: FutureBuilder<List<Map<String, dynamic>>>(
-        future: dbHelper.getPaymentHistoryByAgent(widget.agentID),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Erreur: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(
-                child: Text('Aucun historique de paiement trouvé.'));
-          }
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: dbHelper.getPaymentHistoryByAgent(widget.agentID),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Erreur: ${snapshot.error}'));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(
+              child: Text('Aucun historique de paiement trouvé.'));
+        }
 
-          final paymentHistory = snapshot.data!;
+        final paymentHistory = snapshot.data!;
 
-          return ListView.builder(
-            itemCount: paymentHistory.length,
-            itemBuilder: (context, index) {
-              final payment = paymentHistory[index];
-              return ListTile(
-                title: Text('Montant: ${payment['amount_recu']}'),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Client: ${payment['client_name']}'),
-                    Text('Taxe: ${payment['tax_amount']}'),
-                    Text('Agent: ${payment['agent_name']}'),
-                    Text('Date: ${payment['created_at']}'),
-                  ],
-                ),
-                isThreeLine: true,
-              );
-            },
-          );
-        },
-      ),
+        return ListView.builder(
+          itemCount: paymentHistory.length,
+          itemBuilder: (context, index) {
+            final payment = paymentHistory[index];
+            return ListTile(
+              title: Text('Montant: ${payment['amount_recu']}'),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Client: ${payment['client_name']}'),
+                  Text('Taxe: ${payment['tax_amount']}'),
+                  Text('Date: ${payment['created_at']}'),
+                ],
+              ),
+              isThreeLine: true,
+            );
+          },
+        );
+      },
     );
   }
 }
